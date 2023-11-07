45a4457b62b72fc4b69c01f46547bca5dcb7c91a
charJKL <11600464+charJKL@users.noreply.github.com>
2023-11-07 20:03:43 +0100

    Implement extended `Map.getUniqueId()` method.

--- native/Map.ts
+++ native/Map.ts
@@ -1,10 +1,18 @@
 interface Map<K, V>
 {
 	items(): V[];
+	getUniqueId(): string;
 }
 Map.prototype.items = function<V>() : V[]
 {
 	const list: V[] = [];
 	this.forEach(value => list.push(value));
 	return list;
+}
+Map.prototype.getUniqueId = function() : string
+{
+	const generateId = () => Date.now().toString(36) + Math.random().toString(36).substring(2);
+	let id = generateId();
+	while(this.has(id)) id = generateId();
+	return id;
 }
