1ac1e9b3a4b89f4d846f3fd7b24f11c7ce54a8d4
charJKL <11600464+charJKL@users.noreply.github.com>
2023-11-07 20:04:49 +0100

    Implement extended `Date.getTimestamp()` method.

---
+++ native/Date.ts
@@ -0,0 +1,12 @@
+interface DateConstructor
+{
+	getTimestamp() : number;
+}
+interface Date
+{
+	getTimestamp() : number;
+}
+Date.getTimestamp = function() : number
+{
+	return Math.floor(Date.now() / 1000); 
+}
