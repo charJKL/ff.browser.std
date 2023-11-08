2b88d3d82335b18a763170ecca3044972e64b202
charJKL <11600464+charJKL@users.noreply.github.com>
2023-11-08 22:26:03 +0100

    Refactor `Message` class.

--- api/Message.ts
+++ api/Message.ts
@@ -1,47 +1,52 @@
 import { MessageFailure } from "./MessageFailure";
-import { isFailure } from "./isFailure";
 
 export type MessageSender = browser.runtime.MessageSender;
-export type MessageListener<R> = (sender: MessageSender, ...args: any[]) => R;
 export type MessageVariant = string;
-export type MessageRequest<V extends MessageVariant> = { variant: V, data: any };
-export type MessageResponse<V extends MessageVariant> = { response: "Result" | "Failure", data: any };
+export type MessageListener = (sender: MessageSender, ...args: any[]) => any;
+export type NotificationVariant = string;
+export type NotificationData = object;
 
-export interface MessageList { [key: MessageVariant]: MessageListener<any> };
+export type Function = (...args: any) => any;
+export type RemoveFirstParameter<T extends []> = T extends [arg0: any, ...args: infer ARGS] ? ARGS : T;
+export type ResolveMessageArgs<T extends Function> = RemoveFirstParameter<Parameters<T>>;
+export type ResolveMessageResponse<T extends Function> = Promise<Awaited<ReturnType<T>>>;
+
+export interface SupportedMessages { [variant: MessageVariant]: MessageListener };
+export interface SupportedNotifications { [variant: NotificationVariant]: NotificationData };
+
+export type MessagePacket = { variant: MessageVariant, data: any[] };
+export type MessageResponsePacket = { status: "Success" | "Failure", data: any };
 
 export class Message
 {
-	public static pack(result: any) : MessageResponse<any>
+	public static prepare(variant: any, args: any[]) : MessagePacket
+	{
+		return { variant: variant, data: args };
+	}
+	
+	public static pack(result: any) : MessageResponsePacket
 	{
 		// https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Chrome_incompatibilities#data_cloning_algorithm
 		const isFailure = (value: any) : value is MessageFailure<any, any> => value instanceof MessageFailure;
 		if(isFailure(result))
 		{
-			const serializedFailure = this.SerializeFailure(result);
-			console.log(serializedFailure);
-			return { response: "Failure", data: serializedFailure }
+			return { status: "Failure", data: this.serializeFailure(result) }
 		}
-		return { response: "Result", data: result };
+		return { status: "Success", data: result };
 	}
 	
-	public static unpack(result: MessageResponse<any>): any
+	private static serializeFailure(failure: Object) : {}
 	{
-		if(result.response == "Failure")
-		{
-			return {};
-			//return new MessageFailure(result.data.message);
-		}
-		if(result.response == "Result")
-		{
-			return result.data;
-		}
+		return {error: "fail"}; // TODO serialize error 
 	}
 	
-	private static SerializeFailure(failure: Object) : {}
+	public static unpack(response: MessageResponsePacket): any
 	{
-		console.log("SerializeFailure->start");
-		return Object.entries(failure).reduce((prev, current) => { console.log(prev, current); return prev; }, {instance: failure.constructor.name});
-		console.log("SerializeFailure->end");
-		//return Object.entries(failure).reduce((prev, current) => prev[current[0]] = current[1], {instance: failure.constructor.name});
+		const isFailure = (value: MessageResponsePacket) => value.status == "Failure";
+		if(isFailure(response))
+		{
+			return {}; // TODO unserialize error
+		}
+		return response.data;
 	}
 }
--- api/MessageFailure.ts
+++ api/MessageFailure.ts
@@ -5,7 +5,7 @@ export type MessageFailureInfo = any;
 
 export class MessageFailure<V extends MessageFailureVariant, I extends MessageFailureInfo> extends TypedError<V, I>
 {
-	private scope = "MessageFailureScope" as const; // to differentiate from other errors inherited from `TypeError`, important because of duck typing.
+	private scope = "MessageFailureScope" as const; // to differentiate from other errors inherited from `TypedError`, important because of duck typing.
 	
 	constructor(variant: V, message: string, info: I)
 	{
