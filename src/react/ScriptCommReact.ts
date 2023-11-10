025b8ac059f55fd40a87c6c5cae7f046e0b21020
charJKL <11600464+charJKL@users.noreply.github.com>
2023-11-10 18:19:46 +0100

    Refactor `ScriptCommReact` to conform changes in `ScriptComm`.

--- src/react/ScriptCommReact.ts
+++ src/react/ScriptCommReact.ts
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { ScriptComm } from "../api/script/ScriptComm";
-import { SupportedMessages, SupportedNotifications, ResolveMessageArgs, ResolveMessageResponse } from "../api/Message";
+import { SupportedMessages, SupportedNotifications } from "../api/Message";
 import { isNotUndefined } from "../ex/isUndefined";
 
 export class ScriptCommReact<SM extends SupportedMessages, SN extends SupportedNotifications>
@@ -12,7 +12,7 @@ export class ScriptCommReact<SM extends SupportedMessages, SN extends SupportedN
 		this.$scriptComm = scriptComm;
 	}
 	
-	public useMessage<V extends keyof SM>(variant: V, ...args: ResolveMessageArgs<SM[V]>) : [boolean, null | ReturnType<SM[V]>]
+	public useMessage<V extends keyof SM>(variant: V, ...args: Parameters<SM[V]>) : [boolean, null | ReturnType<SM[V]>]
 	{
 		// TODO here should be some cache, for not request twice the same.
 		const [isComplete, setIsComplete] = useState(false);
@@ -35,21 +35,21 @@ export class ScriptCommReact<SM extends SupportedMessages, SN extends SupportedN
 		return [isComplete, data];
 	}
 	
-	public buildMessage<V extends keyof SM>(variant: V, ...args: ResolveMessageArgs<SM[V]>) : MessageRequest<SM, ScriptComm<SM, SN>, V, ResolveMessageArgs<SM[V]>>
+	public buildMessage<V extends keyof SM>(variant: V, ...args: Parameters<SM[V]>) : MessageRequest<SM, V, Parameters<SM[V]>, ReturnType<SM[V]>>
 	{
 		return new MessageRequest(this.$scriptComm, variant, args);
 	}
 }
 
-class MessageRequest<SM extends SupportedMessages, SC extends ScriptComm<SM, any>, V extends keyof SM, VR>
+class MessageRequest<SM extends SupportedMessages, V extends keyof SM, VArgs extends Parameters<SM[V]>, VResult>
 {
-	private $scriptComm: SC;
+	private $scriptComm: ScriptComm<SM, any>;
 	private $variant: V;
-	private $args: VR;
+	private $args: VArgs;
 	private $before: Function | undefined;
 	private $then : Function | undefined;
 	
-	public constructor(scriptComm: SC, variant: V, args: VR)
+	public constructor(scriptComm: ScriptComm<SM, any>, variant: V, args: VArgs)
 	{
 		this.$scriptComm = scriptComm;
 		this.$variant = variant;
@@ -61,7 +61,7 @@ class MessageRequest<SM extends SupportedMessages, SC extends ScriptComm<SM, any
 		this.$before = before;
 		return this;
 	}
-	public then(then: (args: ResolveMessageResponse<SM[V]>) => void) : this
+	public then(then: (args: VResult) => void) : this
 	{
 		this.$then = then;
 		return this;
@@ -69,7 +69,7 @@ class MessageRequest<SM extends SupportedMessages, SC extends ScriptComm<SM, any
 	public async send() : Promise<void>
 	{
 		if(isNotUndefined(this.$before)) this.$before();
-		const result = await this.$scriptComm.sendMessage(this.$variant, ...this.$args as ResolveMessageArgs<SM[V]>);
+		const result = await this.$scriptComm.sendMessage(this.$variant, ...this.$args);
 		if(isNotUndefined(this.$then)) this.$then(result);
 	}
 }
