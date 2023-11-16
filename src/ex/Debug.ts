7c8043bb4044b4bd37767e2d5ef89ef152156c02
charJKL <11600464+charJKL@users.noreply.github.com>
2023-11-16 16:07:40 +0100

    Implement `Debug.logFunction()` method.

--- src/ex/Debug.ts
+++ src/ex/Debug.ts
@@ -1,5 +1,52 @@
+import "../native/String";
+
 export class Debug 
 {
+	public static Script = Symbol("Debug.Format.Script");
+	public static Background = Symbol("Debug.Format.Background");
+	
+	private static Formats = 
+	{
+		[Debug.Script]: "background: rgba(247, 220, 111, 1);",
+		[Debug.Background]: "background: rgba(247, 220, 111, 1);"
+	}
+	
+	public logFunction(format: keyof typeof Debug.Formats, msg: string, ...args: any[])
+	{
+		// TODO refactor it to use Error.Stack 
+		const styles = Debug.Formats[format];
+		const [rawFunctionName, rawVarsList] = msg.find("=$").findBefore(" ").cut();
+		const functionName = rawFunctionName.trim().trimSuffix(",");
+		const varsList = rawVarsList.replaceAll(/\$[0-9]/g, "%o");
+		const vars = this.transformArgs(args);
+		console.group(`%c${functionName}%c ${varsList}`, styles, "", ...vars);
+	}
+	
+	public endFunction() : void
+	{
+		console.groupEnd();
+	}
+	
+	private transformArgs(args: any[]) : any[]
+	{
+		return args.map(a => this.transformType(a));
+	}
+	
+	private transformType(argument: any) : any
+	{
+		switch(typeof argument)
+		{
+			case "undefined": return argument;
+			case "object": return argument;
+			case "boolean": return new Boolean(argument);
+			case "number": return new Number(argument);
+			case "bigint": return BigInt(argument);
+			case "string": return new String(argument);
+			case "symbol": return argument;
+			case "function": return argument;
+		}
+	}
+	
 	public clear() : void
 	{
 		console.clear();
