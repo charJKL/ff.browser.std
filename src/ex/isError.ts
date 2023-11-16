fedf5c6ab81b04bb12aa7b36f1294b299539493c
charJKL <11600464+charJKL@users.noreply.github.com>
2023-11-16 09:55:49 +0100

    Fix typo in `isError` method.

--- src/ex/isError.ts
+++ src/ex/isError.ts
@@ -2,7 +2,7 @@ import { TypedError, TypedErrorVariant, TypedErrorInfo } from "./TypedError";
 
 export function isError<V1 extends symbol, V2 extends Exclude<TypedErrorVariant, V2>, R>(variant: symbol, error: R | TypedError<V2, any>) : error is TypedError<V2, any>;
 export function isError<V1 extends V2, V2 extends Exclude<TypedErrorVariant, V2>, R>(variant: V1, error: R | TypedError<V2, any>): error is TypedError<V1, any>;
-export function isError<V1 extends V2, V2 extends TypedErrorVariant, R>(variant: V1 | symbol, error: R | TypedError<V2, any>): error is TypedError<V1, any>
+export function isError<V1 extends V2, V2 extends Exclude<TypedErrorVariant, V2>, R>(variant: V1 | symbol, error: R | TypedError<V2, any>): error is TypedError<V1, any>
 {
 	if(error instanceof TypedError && variant === TypedError.Any) return true;
 	if(error instanceof TypedError && variant === error.variant) return true;
