0decd29a68bb7647d4e56599134550edf336a00a
charJKL <11600464+charJKL@users.noreply.github.com>
2023-11-16 09:57:19 +0100

    Implement `isFailure` functions restrictions.

--- src/api/isFailure.ts
+++ src/api/isFailure.ts
@@ -1,7 +1,11 @@
-import { MessageFailure, MessageFailureVariant, MessageFailureInfo } from "./MessageFailure";
+import { TypedError } from "../ex/TypedError";
+import { MessageFailure, MessageFailureVariant } from "./MessageFailure";
 
-export function isFailure<V extends MessageFailureVariant, I extends MessageFailureInfo>(variant: V, failure: any) : failure is MessageFailure<V, I>
+export function isFailure<V1 extends symbol, V2 extends Exclude<MessageFailureVariant, V2>, R>(variant: symbol, failure: R | TypedError<V2, any>): failure is TypedError<V2, any>;
+export function isFailure<V1 extends V2, V2 extends Exclude<MessageFailureVariant, V2>, R>(variant: V1, failure: R | TypedError<V2, any>): failure is TypedError<V1, any>;
+export function isFailure<V1 extends V2, V2 extends Exclude<MessageFailureVariant, V2>, R>(variant: V1 | symbol, failure: R | TypedError<V2, any>): failure is TypedError<V1, any>
 {
-	if(failure instanceof MessageFailure && failure.variant === variant) return true;
+	if(failure instanceof MessageFailure && variant === MessageFailure.Any) return true;
+	if(failure instanceof MessageFailure && variant === failure.variant) return true;
 	return false;
 }
