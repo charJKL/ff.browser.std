91ab9a14aaa4ec231d10fbd2d73fca5f0fcf24d8
charJKL <11600464+charJKL@users.noreply.github.com>
2023-11-17 11:13:03 +0100

    Extend native `Array` with `sortAsNumber()`.

--- src/native/Array.ts
+++ src/native/Array.ts
@@ -2,6 +2,7 @@ interface Array<T>
 {
 	contains(item: T): boolean;
 	notContains(item: T) : boolean;
+	sortAsNumbers() : Array<T>;
 }
 Array.prototype.contains = function<T>(item: T) : boolean
 {
@@ -10,4 +11,8 @@ Array.prototype.contains = function<T>(item: T) : boolean
 Array.prototype.notContains = function<T>(item: T) : boolean
 {
 	return this.includes(item) == false;
+}
+Array.prototype.sortAsNumbers = function<T>() : Array<T>
+{
+	return this.sort((a, b) => a - b);
 }
