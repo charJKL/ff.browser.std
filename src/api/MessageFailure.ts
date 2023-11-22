6154950a0815e6f51d14a1791a51243e43c2d349
charJKL <11600464+charJKL@users.noreply.github.com>
2023-11-22 16:08:09 +0100

    Split `MessageFailure` from `TypedError`.

--- src/api/MessageFailure.ts
+++ src/api/MessageFailure.ts
@@ -1,14 +1,35 @@
-import { TypedError } from "../classes/TypedError";
-
 export type MessageFailureVariant = string;
 export type MessageFailureInfo = any;
 
-export class MessageFailure<V extends MessageFailureVariant, I extends MessageFailureInfo> extends TypedError<V, I> // TODO should message extend TypedError? I don't thing so.
+export class MessageFailure<V extends MessageFailureVariant, I extends MessageFailureInfo>
 {
 	private scope = "MessageFailureScope" as const; // to differentiate from other errors inherited from `TypedError`, important because of duck typing.
 	
+	public static Any = Symbol("AnyPossibleFailure");
+	private $variant: V;
+	private $message: string;
+	private $info: I;
+	
 	constructor(variant: V, message: string, info: I)
 	{
-		super(variant, message, info, null);
+		this.$variant = variant;
+		this.$message = message;
+		this.$info = info;
+	}
+	
+	public get variant() : V
+	{
+		return this.$variant;
+	}
+	
+	public get message(): string
+	{
+		return this.$message;
+	}
+	
+	public get info(): I
+	{
+		return this.$info;
 	}
 }
+
--- src/api/isFailure.ts
+++ src/api/isFailure.ts
@@ -1,9 +1,8 @@
-import { TypedError } from "../classes/TypedError";
 import { MessageFailure, MessageFailureVariant } from "./MessageFailure";
 
-export function isFailure<V1 extends symbol, V2 extends Exclude<MessageFailureVariant, V2>, R>(variant: symbol, failure: R | TypedError<V2, any>): failure is TypedError<V2, any>;
-export function isFailure<V1 extends V2, V2 extends Exclude<MessageFailureVariant, V2>, R>(variant: V1, failure: R | TypedError<V2, any>): failure is TypedError<V1, any>;
-export function isFailure<V1 extends V2, V2 extends Exclude<MessageFailureVariant, V2>, R>(variant: V1 | symbol, failure: R | TypedError<V2, any>): failure is TypedError<V1, any>
+export function isFailure<V1 extends symbol, V2 extends Exclude<MessageFailureVariant, V2>, R>(variant: symbol, failure: R | MessageFailure<V2, any>): failure is MessageFailure<V2, any>;
+export function isFailure<V1 extends V2, V2 extends Exclude<MessageFailureVariant, V2>, R>(variant: V1, failure: R | MessageFailure<V2, any>): failure is MessageFailure<V1, any>;
+export function isFailure<V1 extends V2, V2 extends Exclude<MessageFailureVariant, V2>, R>(variant: V1 | symbol, failure: R | MessageFailure<V2, any>): failure is MessageFailure<V1, any>
 {
 	if(failure instanceof MessageFailure && variant === MessageFailure.Any) return true;
 	if(failure instanceof MessageFailure && variant === failure.variant) return true;
