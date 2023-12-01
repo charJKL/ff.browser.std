a4f728a53c7628597779e91d729fdb50b4da2aa4
charJKL <11600464+charJKL@users.noreply.github.com>
2023-12-01 15:04:37 +0100

    Implement `hasProp` method.

---
+++ src/functions/hasProp.ts
@@ -0,0 +1,7 @@
+import { isNotNull } from "./isNull";
+import { isNotUndefined } from "./isUndefined";
+
+export function hasProp<S extends string>(value: any, prop: S ) : value is {[Key in S]: unknown}
+{
+	return isNotUndefined(value) && isNotNull(value) && isNotUndefined(value.prop);
+}
