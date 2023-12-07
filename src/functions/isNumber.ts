4d0cf76d57ec7a87e59d47210ce8ec6b3493ef07
charJKL <11600464+charJKL@users.noreply.github.com>
2023-12-07 19:03:51 +0100

    Implement `isNumber` methods.

---
+++ src/functions/isNumber.ts
@@ -0,0 +1,11 @@
+
+
+
+export function isNumber(value: any) : value is number
+{
+	return typeof value === "number";
+}
+export function isNotNumber(value: any) : value is unknown
+{
+	return typeof value !== "number";
+}
