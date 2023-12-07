4293b6a861d0721ed0ae24b5e6115e46782e3e1b
charJKL <11600464+charJKL@users.noreply.github.com>
2023-12-07 19:20:34 +0100

    Change `Map` extension methods.

--- src/native/Map.ts
+++ src/native/Map.ts
@@ -1,19 +1,13 @@
 interface Map<K, V>
 {
 	hasNot(key: K) : boolean;
-	items(): Array<V>;
 	getUniqueId(): string;
-	transform<R>(func: (key: K, value: V) => R) : Array<R>;
+	castTo<R>(func: (key: K, value: V) => R) : Map<K, R>;
+	toArray<R>(func: (key: K, value: V) => R) : Array<R>;
 }
 Map.prototype.hasNot= function<K>(key: K) : boolean
 {
 	return this.has(key) === false;
-}
-Map.prototype.items = function<V>() : Array<V>
-{
-	const list: Array<V> = [];
-	this.forEach(value => list.push(value));
-	return list;
 }
 Map.prototype.getUniqueId = function() : string
 {
@@ -22,7 +16,12 @@ Map.prototype.getUniqueId = function() : string
 	while(this.has(id)) id = generateId();
 	return id;
 }
-Map.prototype.transform = function<K, V, R>(func: (key: K, value: V) => R) : Array<R>
+Map.prototype.castTo = function<K, V, R>(func: (key: K, value: V) => R) : Map<K, R>
+{
+	this.forEach((value, key) => this.set(key, func(key, value)));
+	return this;
+}
+Map.prototype.toArray = function<K, V, R>(func: (key: K, value: V) => R) : Array<R>
 {
 	const list: Array<R> = [];
 	this.forEach((value, key) => list.push(func(key, value)));
