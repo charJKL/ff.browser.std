d838908ae8686e2b7f803bb1ccaf1fde169603c0
charJKL <11600464+charJKL@users.noreply.github.com>
2023-12-11 20:02:20 +0100

    Fix eslint `@typescript-eslint/no-explicit-any` rule.

--- src/ex/isError.ts
+++ src/ex/isError.ts
@@ -1,8 +1,8 @@
 import { TypedError, TypedErrorVariant } from "./TypedError";
 
-export function isError<V2 extends Exclude<TypedErrorVariant, V2>, R>(variant: symbol, error: R | TypedError<V2, unknown>) : error is TypedError<V2, any>;
+export function isError<V2 extends Exclude<TypedErrorVariant, V2>, R>(variant: symbol, error: R | TypedError<V2, unknown>) : error is TypedError<V2, unknown>;
 export function isError<V1 extends V2, V2 extends Exclude<TypedErrorVariant, V2>, R>(variant: V1, error: R | TypedError<V2, unknown>): error is TypedError<V1, unknown>;
-export function isError<V1 extends V2, V2 extends Exclude<TypedErrorVariant, V2>, R>(variant: V1 | symbol, error: R | TypedError<V2, unknown>): error is TypedError<V1, any>
+export function isError<V1 extends V2, V2 extends Exclude<TypedErrorVariant, V2>, R>(variant: V1 | symbol, error: R | TypedError<V2, unknown>): error is TypedError<V1, unknown>
 {
 	if(error instanceof TypedError && variant === TypedError.Any) return true;
 	if(error instanceof TypedError && variant === error.variant) return true;
