b8530179b67caf86ba1e041681bed8c10b6dadff
charJKL <11600464+charJKL@users.noreply.github.com>
2023-12-11 11:38:55 +0100

    Add missing function pair for `functions`.

--- src/functions/isObject.ts
+++ src/functions/isObject.ts
@@ -2,3 +2,7 @@ export function isObject(value: any) : value is object
 {
 	return typeof value === "object";
 }
+export function isNotObject(value: any) : value is object
+{
+	return typeof value !== "object";
+}
