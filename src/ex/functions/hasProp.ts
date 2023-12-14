df61638481a2f73b8c06bc2f45595bfedc49c164
charJKL <11600464+charJKL@users.noreply.github.com>
2023-12-14 13:30:56 +0100

    Fix bug with `hasProp` implementation.

--- src/ex/functions/hasProp.ts
+++ src/ex/functions/hasProp.ts
@@ -3,5 +3,5 @@ import { isNotUndefined } from "./isUndefined";
 
 export function hasProp<S extends string>(value: any, prop: S ) : value is {[Key in S]: unknown}
 {
-	return isNotUndefined(value) && isNotNull(value) && isNotUndefined(value.prop);
+	return isNotUndefined(value) && isNotNull(value) && isNotUndefined(value[prop]);
 }
