6f3e19f84174665e548ef9e579a98f12f42def8b
charJKL <11600464+charJKL@users.noreply.github.com>
2023-12-14 09:47:32 +0100

    Remove native objects extensions.

--- build.mjs
+++ build.mjs
@@ -1,10 +1,6 @@
 import esbuild from "esbuild";
-import { glob } from "glob";
 import { dtsPlugin } from "esbuild-plugin-d.ts";
 import { clean } from "esbuild-plugin-clean";
-
-
-const natives = await glob("./src/ex/native/*");
 
 // TODO Okey but now how to setup tsc --noEmit for type checking?
 // TODO create second script for watch, and share configuration between both
@@ -15,8 +11,7 @@ const ctx1 = await esbuild.context({
 		"./src/ex.ts",
 		"./src/frontend.react.ts",
 		"./src/frontend.ts",
-		"./src/index.ts",
-		...natives,
+		"./src/index.ts"
 	],
 	external: ["react"],
 	outdir: "./dist/",
